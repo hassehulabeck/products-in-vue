63d98931376f39f370e8547a6b9e03b2aa7444b0
hassehulabeck <hansbudandersson@gmail.com>
2021-02-11 14:10:41 +0100

    Added mostly skeletons

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: []
   },
   mutations: {
+    GET_PRODUCTS(products) {
+      this.state.products = products
+    }
   },
   actions: {
+    getProducts({commit}) {
+      axios.get('https://www.hulabeck.se/html/temp/products.json')
+        .then((response) => {
+          commit('GET_PRODUCTS', response.data.products)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Välkommen till min butik</h1>
+    <Product />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Product from "@/components/Product.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Product,
+  },
+  mounted() {
+    this.$store.dispatch("getProducts");
+  },
+};
 </script>
